632e81305231377548f2b7746701e3272dffac8e
unknown <you@example.com>
2024-09-17 14:52:27 +0300

    Firts

---
+++ resources/views/posts/edit.blade.php
@@ -0,0 +1,16 @@
+<x-app-layout>
+    <h1>Edit post</h1>
+
+    <form action="{{ route('posts.update', $post->id) }}" method="POST">
+        @csrf
+        @method('PUT')
+
+        <label for="title">Title: </label>
+        <input type="text" id="title" name="title" value="{{$post->title}}"> 
+        <br>
+        <label for="content">Content: </label>
+        <textarea name="content" id="content">{{ $post->content }}</textarea>
+        <br>
+        <input type="submit" value="Update">
+    </form>
+</x-app-layout>
---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,22 @@
+<x-app-layout>
+    <h1>All posts</h1>
+    <ul>
+        @foreach($posts as $post)
+            <li>
+                <h2>Title: {{ $post->title }}</h2>
+                <p>Content: {{ $post->content }}</p>
+                <div>
+                    <a href="{{ route('posts.show', $post->id) }}">Show</a>
+                    <a href="{{ route('posts.edit', $post->id) }}">Edit</a>
+                    <form action="{{ route('posts.destroy', $post->id) }}" method="POST" class="inline-block">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+                            Delete
+                        </button>
+                    </form>
+                </div>
+            </li>
+        @endforeach
+    </ul>
+</x-app-layout>
